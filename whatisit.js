d923f88cfa8a20aa27a4c75922753a13dde898fc
Erik Tyler Villoria <evilloria@gmail.com>
2016-07-15 18:00:04 +0000

    add HW base

--- whatisit.js
+++ whatisit.js
@@ -1,1 +1,11 @@
 // request header parser microservice
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('woo-hoo');
+});
+
+app.listen(8080, function () {
+  console.log('app listening on port 8080');
+});
