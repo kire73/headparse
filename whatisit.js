9e8cc36d59c7fe6ecfb37eb2dc28f59dd9477cf3
Erik Tyler Villoria <evilloria@gmail.com>
2016-09-13 23:54:04 +0000

    fixed port

--- whatisit.js
+++ whatisit.js
@@ -17,6 +17,6 @@ app.get('/', function (req, res) {
   res.send(all);
 });
 
-app.listen(8080, function () {
-  console.log('app listening on port 8080');
+app.listen(process.env.PORT, function () {
+  console.log('app listening on port ' + process.env.PORT );
 });
